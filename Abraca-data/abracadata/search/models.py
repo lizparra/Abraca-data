40d8afd4d81c5fe76aa51087568d1749eab2da1a
lizparra <44618559+lizparra@users.noreply.github.com>
2019-11-28 01:37:51 -0500

    In progress of completing the Search app where I have added a Pet Model so far - Liz

---
+++ Abraca-data/abracadata/search/models.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models
+
+# Create your models here.
+
+
+class Pet(models.Model):
+    name = models.CharField(max_length=45)
+    age = models.IntegerField()
+    sex = models.IntegerField()
+    spayed = models.BooleanField()
+    size = models.CharField(max_length=15)
+    color = models.CharField(max_length=45)
+    fostered = models.BooleanField()
+    acquired_date = models.DateField(auto_now_add=True)
+    breed_id_fk = models.IntegerField()
+    image = models.CharField(max_length=250)
